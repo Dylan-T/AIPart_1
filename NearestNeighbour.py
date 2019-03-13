17aeeef8dd1f794f4ff697236b4631b57eb02da3
thomsodyla1 <thomsodyla1@myvuw.ac.nz>
2019-03-14 10:43:10 +1300

    Wrote distance calculation and data set up code

--- NearestNeighbour.py
+++ NearestNeighbour.py
@@ -1,5 +1,19 @@
+import numpy
+from scipy.spatial import distance
+
 """Load the data"""
-trainingData = open("part1/iris-training.txt", "r")
+trainingText = open("part1/iris-training.txt", "r")
+trainingData = []
+for line in trainingText:
+    tokens = line.split()
+    trainingData.append(numpy.array((float(tokens[0]), float(tokens[1]), float(tokens[2]), float(tokens[3]))))
+
+
+testingText = open("part1/iris-test.txt", "r")
+testingData = []
+for line in testingText:
+    tokens = line.split()
+    testingData.append(numpy.array((float(tokens[0]), float(tokens[1]), float(tokens[2]), float(tokens[3]))))
 
 """Initialise the value of k"""
 k = 1
@@ -10,6 +24,10 @@ for x in range(1, 10):
     """Calculate the distance between test data and each row of training data. Here we will use Euclidean distance as 
     our distance metric since it’s the most popular method. The other metrics that can be used are Chebyshev, cosine, 
     etc. """
+    dist = numpy.linalg.norm(testingData[0]-trainingData[0])
+    dst = distance.euclidean(testingData[0], trainingData[0])
+    print(dst)
+    print(dist)
 
     """Sort the calculated distances in ascending order based on distance values"""
 
@@ -18,3 +36,5 @@ for x in range(1, 10):
     """Get the most frequent class of these rows"""
 
     """Return the predicted class"""
+
+
