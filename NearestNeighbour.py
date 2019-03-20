ba9942cbb03555847c62a2555684e6b839c8d61c
thomsodyla1 <thomsodyla1@myvuw.ac.nz>
2019-03-21 10:36:01 +1300

    Remote had been broken and all commits had been failing, Pushing a working KNN algorithm

--- NearestNeighbour.py
+++ NearestNeighbour.py
@@ -1,40 +1,61 @@
 import numpy
 from scipy.spatial import distance
+import operator
+import sys
 
 """Load the data"""
-trainingText = open("part1/iris-training.txt", "r")
+if len(sys.argv) > 1:
+    trainingText = open(sys.argv[1], "r")
+else:
+    trainingText = open("part1/iris-training.txt")
+
 trainingData = []
 for line in trainingText:
     tokens = line.split()
-    trainingData.append(numpy.array((float(tokens[0]), float(tokens[1]), float(tokens[2]), float(tokens[3]))))
+    trainingData.append((numpy.array((float(tokens[0]), float(tokens[1]), float(tokens[2]), float(tokens[3]))), tokens[4])) #in format [((vector), class), ((vector), class)...((vector), class)]
 
+if len(sys.argv) > 1:
+    testingText = open(sys.argv[2], "r")
+else:
+    testingText = open("part1/iris-test.txt")
 
-testingText = open("part1/iris-test.txt", "r")
 testingData = []
 for line in testingText:
     tokens = line.split()
-    testingData.append(numpy.array((float(tokens[0]), float(tokens[1]), float(tokens[2]), float(tokens[3]))))
+    testingData.append((numpy.array((float(tokens[0]), float(tokens[1]), float(tokens[2]), float(tokens[3]))), tokens[4])) #in format [((vector), class), ((vector), class)...((vector), class)]
 
 """Initialise the value of k"""
-k = 1
+k = 5
+predictions = []
 
-"""For getting the predicted class, iterate from 1 to total number of training data points"""
-for x in range(1, 10):
+"""Do this for each test point"""
+for test in range(len(testingData)):
 
-    """Calculate the distance between test data and each row of training data. Here we will use Euclidean distance as 
-    our distance metric since it’s the most popular method. The other metrics that can be used are Chebyshev, cosine, 
-    etc. """
-    dist = numpy.linalg.norm(testingData[0]-trainingData[0])
-    dst = distance.euclidean(testingData[0], trainingData[0])
-    print(dst)
-    print(dist)
+    """Calculate the distance between test data and each row of training data."""
+    distances = []
+    for train in range(len(trainingData)):
+        dist = numpy.linalg.norm(testingData[test][0]-trainingData[train][0])
+        distances.append([dist,trainingData[train][1]])
+
 
     """Sort the calculated distances in ascending order based on distance values"""
+    distances.sort()
 
     """Get top k rows from the sorted array"""
-
     """Get the most frequent class of these rows"""
+    classes = {}
 
-    """Return the predicted class"""
+    for x in range(k):
+        if distances[x][1] in classes:
+            classes[distances[x][1]] = classes[distances[x][1]]+1
+        else:
+            classes[distances[x][1]] = 1
 
+    """Add predicted class to list"""
+    predictions.append( max(classes, key=classes.get))
 
+"""write predictions to a txt file"""
+f=open("resultsK"+str(k)+".txt", "w+")
+for i in predictions:
+    f.write(i + "\n")
+f.close()
